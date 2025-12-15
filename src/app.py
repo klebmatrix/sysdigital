c9f6405ec0d3854927fc8151d96059a5a040a10b
kleber Lima <klebelim@gmail.com>
2025-12-15 11:01:26 -0300

    Adiciona arquivos e pastas novas

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
-# Carrega variáveis de ambiente do .env (se houver)
 load_dotenv()
 
 app = Flask(__name__)
@@ -12,5 +11,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # Roda localmente
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
