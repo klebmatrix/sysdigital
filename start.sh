dfde5d79a0c03c279080485b7f645556e9065f11
kleber Lima <klebelim@gmail.com>
2025-12-15 10:12:22 -0300

    Adiciona arquivos e pastas novas

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
-gunicorn app:app
+@echo off
+REM Ativa o ambiente virtual
+call venv\Scripts\activate
 
+REM Define variáveis de ambiente
+set FLASK_APP=app.py
+set FLASK_ENV=development
 
+REM Roda a aplicação
+flask run
+pause
